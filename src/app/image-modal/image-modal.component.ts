e3f21104dab1f6900638f1accca585466313f4e5
juanito mac <jean28tiago@gmail.com>
2023-04-29 19:06:00 +0200

    Ajout de la suprresion de l'image part 1 et modification du scss des pages

--- src/app/image-modal/image-modal.component.ts
+++ src/app/image-modal/image-modal.component.ts
@@ -1,23 +1,15 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-image-modal',
-  template: `
-    <h2 mat-dialog-title>{{ data.name }}</h2>
-    <mat-dialog-content>
-      <img [src]="data.url" [alt]="data.name" class="modal-image" />
-      <h3>{{ data.description }}</h3>
-      <h3>{{ data.tags }}</h3>
-
-    </mat-dialog-content>
-    <mat-dialog-actions>
-      <button mat-button (click)="onClose()">Fermer</button>
-    </mat-dialog-actions>
-  `,
+  templateUrl: './image-modal.component.html',
+  styleUrls: ['./image-modal.component.scss']
 })
 export class ImageModalComponent {
   constructor(
+    private http: HttpClient,
     public dialogRef: MatDialogRef<ImageModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
@@ -25,4 +17,30 @@ export class ImageModalComponent {
   onClose(): void {
     this.dialogRef.close();
   }
+
+  // onDelete(imageName: string): void {
+  //   this.http.delete(`/delete_image/${imageName}`).subscribe(
+  //     (response) => {
+  //       console.log('Image supprimée avec succès');
+  //       // Fermez la fenêtre modale et actualisez la liste des images si nécessaire
+  //     },
+  //     (error) => {
+  //       console.error('Erreur lors de la suppression de l\'image');
+  //     }
+  //   );
+  //   this.dialogRef.close('delete');
+  // }
+  onDelete(imageName: string): void {
+    this.http.delete(`/delete_image/${imageName}`).subscribe(
+      (response) => {
+        console.log('Image supprimée avec succès');
+        // Fermez la fenêtre modale et actualisez la liste des images si nécessaire
+      },
+      (error) => {
+        console.error('Erreur lors de la suppression de l\'image');
+      }
+    );
+    this.dialogRef.close('delete');
+  }
+  
 }
