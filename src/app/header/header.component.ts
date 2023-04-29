17a1f4f6489c512229015be2a0c336b8a8453ce7
juanito mac <jean28tiago@gmail.com>
2023-04-29 16:34:33 +0200

    Ajout de la reconnaissance vocale partie 1

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { SharedService } from '../shared.service'; // Ajoutez cette ligne
+import { SharedService } from '../shared.service';
+import { HttpClient } from '@angular/common/http'; // Importez HttpClient
 
 @Component({
   selector: 'app-header',
@@ -7,12 +8,56 @@ import { SharedService } from '../shared.service'; // Ajoutez cette ligne
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  constructor(private sharedService: SharedService) {} // Ajoutez cette ligne
+  constructor(
+    private sharedService: SharedService,
+    private http: HttpClient // Injectez HttpClient
+  ) {}
 
   filterImages(event: KeyboardEvent): void {
     const target = event.target as HTMLInputElement;
     const searchText = target?.value || '';
     this.sharedService.setSearchText(searchText);
-  }  
-  
+  }
+
+  startListening(): void {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then((stream) => {
+        const mediaRecorder = new MediaRecorder(stream);
+        mediaRecorder.start();
+
+        const audioChunks: Blob[] = [];
+        mediaRecorder.addEventListener('dataavailable', (event) => {
+          audioChunks.push(event.data);
+        });
+
+        mediaRecorder.addEventListener('stop', () => {
+          const audioBlob = new Blob(audioChunks, {
+            type: 'audio/webm',
+          });
+
+          // Remplacez l'URL par l'URL de votre serveur Django
+          const apiUrl = 'http://localhost:8000/azure_speech_to_text/';
+
+          const formData = new FormData();
+          formData.append('audio_data', audioBlob, 'audio.webm');
+
+          this.http.post<{ transcript: string }>(apiUrl, formData).subscribe(
+            (response) => {
+              this.sharedService.setSearchText(response.transcript);
+            },
+            (error) => {
+              console.error('Error:', error);
+            }
+          );
+        });
+
+        setTimeout(() => {
+          mediaRecorder.stop();
+        }, 3000);
+      })
+      .catch((err) => {
+        console.error('Error:', err);
+      });
+  }
 }
