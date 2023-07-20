f01d44cb80fbf0f22dfff55e12134849db519a33
juanito mac <jean28tiago@gmail.com>
2023-07-20 11:30:19 +0200

    modification requirements

--- djangoAzure/settings.py
+++ djangoAzure/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 import dj_database_url
 from pathlib import Path
 
@@ -173,7 +173,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
