a0067999e30d0f4f4152cdf7339a9b116a7d8741
juanito mac <jean28tiago@gmail.com>
2023-07-26 15:27:33 +0200

    param déploiement angular heroku

--- serveur.js
+++ serveur.js
@@ -1,9 +1,22 @@
-const express = require ("express");
-const path = require( 'path');
-const app = express();
-app.use (express.static(__dirname + '/dist/angular-azure')) ;
-app.get ('/*', function (req, res) {
-res. sendFile (path.join(__dirname+'/dist/angular-azure/index.html'));
+// const express = require ("express");
+// const path = require( 'path');
+// const app = express();
+// app.use (express.static(__dirname + '/dist/angular-azure')) ;
+// app.get ('/*', function (req, res) {
+// res. sendFile (path.join(__dirname+'/dist/angular-azure/index.html'));
 
-})
-app. listen (process.env.PORT) ;
+// })
+// app. listen (process.env.PORT) ;
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('./dist/angular-azure'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/angular-azure/index.html'}),
+);
+
+app.listen(process.env.PORT || 8080);
