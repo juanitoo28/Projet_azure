7f05ffef9326edd18fc8d995eb0184fc84c7de88
juanito mac <jean28tiago@gmail.com>
2023-07-27 15:54:11 +0200

    Modification final de la reconnaissance vocale et fonctionnel

--- src/environnements/environnement.ts
+++ src/environnements/environnement.ts
@@ -1,12 +1,12 @@
 
-export const environment = {
-  production: true,
-  apiUrl: "http://localhost:8000", // Remplacez ceci par l'URL de votre serveur Django en production
-};
 // export const environment = {
 //   production: true,
-//   apiUrl: "https://newapp2.azurewebsites.net", // Remplacez ceci par l'URL de votre serveur Django en production
+//   apiUrl: "http://localhost:8000", // Remplacez ceci par l'URL de votre serveur Django en production
 // };
+export const environment = {
+  production: true,
+  apiUrl: "https://newapp2.azurewebsites.net", // Remplacez ceci par l'URL de votre serveur Django en production
+};
 // export const environment = {
 //     production: false,
 //     apiUrl: "https://appimg.azurewebsites.net", // Remplacez ceci par l'URL de votre serveur Django en production
