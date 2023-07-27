0abbb020e8b6046f4d49794b465563141d616948
juanito mac <jean28tiago@gmail.com>
2023-07-27 10:19:02 +0200

    param déploiement angular heroku

--- serveur.js
+++ serveur.js
@@ -13,7 +13,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/angular-azure/'));
+app.use(express.static('./dist/angular-azure'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/angular-azure/'}),
