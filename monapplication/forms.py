864a2dfef66f46c6685b05badcee80ff2edbaad3
Jean Tiago <60743021+juanitoo28@users.noreply.github.com>
2023-07-28 15:18:43 +0200

    Delete monapplication directory

--- monapplication/forms.py
+++
@@ -1,8 +0,0 @@
-# monapplication/forms.py
-from django import forms
-from .models import Todo
-
-class TodoForm(forms.ModelForm):
-    class Meta:
-        model = Todo
-        fields = ['titre', 'description']
